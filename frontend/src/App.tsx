f45b07e31ebeb2bbe4bc4a30cb0cc43268ca33d4
Vidyut Venkatesan <s3925040@student.rmit.edu.au>
2023-09-01 02:56:18 +1000

    Added Navbar items

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
 // import React from 'react';
 // import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';
+
 import './App.css';
 import Navbar from './components/navbar/navbar';
+
 
 function App() {
   return (
