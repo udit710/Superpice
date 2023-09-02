8bc27c7081afefafacaf0fc4f3b2a580a25226de
Abhijeet Kumar <s3905291@student.rmit.edu.au>
2023-09-03 05:27:20 +1000

    unzip superprice base code to backend

---
+++ backend/src/test/java/rmit/sept/superprice/SuperPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package rmit.sept.superprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperPriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
