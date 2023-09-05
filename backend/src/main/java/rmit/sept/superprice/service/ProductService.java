92a41c8fa8cad115fed207ca29065713f48c092b
Abhijeet Kumar <s3905291@student.rmit.edu.au>
2023-09-06 06:22:19 +1000

    add Product data

--- backend/src/main/java/rmit/sept/superprice/service/ProductService.java
+++ backend/src/main/java/rmit/sept/superprice/service/ProductService.java
@@ -2,7 +2,10 @@ package rmit.sept.superprice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import rmit.sept.superprice.model.Product;
 import rmit.sept.superprice.repository.ProductRepository;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -10,5 +13,19 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    // TODO: Add service methods
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product saveOrUpdateProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
 }
