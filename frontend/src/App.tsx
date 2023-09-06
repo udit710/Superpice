29f64b889af3694fc1fad2ebc6244d46df44bf1b
ra5h1k <s3931830@student.rmit.edu.au>
2023-09-06 17:13:39 +1000

    Created Layout of Product Detail Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,12 @@
 // import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
+import { Route, Routes } from 'react-router';
+
 
 import './App.css';
 import Navbar from './components/navbar/navbar';
-import PriceCompare from './components/pricecompare/pricecompare';
-import ProductImg from './components/pricecompare/productimg';
+import ProductDetail from './pages/product_detail/ProductDetail';
 
 
 function App() {
@@ -14,10 +15,12 @@ function App() {
     <div className="App">
       <Navbar/>
       <div className='space'></div>
-      <div className="flex hstack">
-      <ProductImg/>
-      <PriceCompare/>
-      </div>
+      
+      {/**IDK how to do next.js routing so i did this for the time being */}
+      <Routes>
+        <Route path='/ProductDetail/:id' element={<ProductDetail/>}/>
+      </Routes>
+      
     </div>
   );
 }
---
+++ frontend/src/components/product_review/ProductReview.tsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+import "./ProductReview.css"
+export default class ProductReview extends Component{
+    render(){
+
+        //creating Fake Review
+        
+
+        return(
+            <div className='product_review_container'>
+                <div className="review_body">
+                    <h4>Person Name</h4>
+                    <p>Lorem ipsum dolor sit amet, cons commodo consequat. Duis 
+                        aute irure dolor in reprehenderit in voluptate velit esse 
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                         cupidatat non proident, sunt in culpa qui officia deserunt 
+                         mollit anim id est laborum.</p>
+                    <hr/>
+                </div>
+            </div>
+        )
+    }
+}
