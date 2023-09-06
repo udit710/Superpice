a08aeacc5d1b6e7baa327bdb09bfa11264a1893f
Abhijeet Kumar <s3905291@student.rmit.edu.au>
2023-09-07 03:11:19 +1000

    remove other types for now

--- backend/src/main/java/au/edu/rmit/sept/superprice/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package au.edu.rmit.sept.superprice.repository;
-
-import au.edu.rmit.sept.superprice.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- backend/src/main/java/au/edu/rmit/sept/superprice/repository/TransactionItemRepository.java
+++
@@ -1,9 +0,0 @@
-package au.edu.rmit.sept.superprice.repository;
-
-import au.edu.rmit.sept.superprice.model.TransactionItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionItemRepository extends JpaRepository<TransactionItem, Long> {
-}
