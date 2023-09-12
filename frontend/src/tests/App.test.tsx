bf05bb1717332d19e5959931bebb846d1e7f6216
Abhijeet Kumar <s3905291@student.rmit.edu.au>
2023-09-12 23:31:04 +1000

    move App.test to test folder

--- frontend/src/tests/App.test.tsx
+++ frontend/src/tests/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import { describe, test, expect } from 'vitest'
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  // render(<App />);
+  const linkElement = screen.getByText(/a/i);
   expect(linkElement).toBeInTheDocument();
 });
