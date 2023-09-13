ffae695c82092adde974f3d897d94ff3074ab120
Abhijeet Kumar <s3905291@student.rmit.edu.au>
2023-09-13 21:13:56 +1000

    update ProductDetail tests

--- frontend/src/tests/productDetails/productDetail.test.tsx
+++ frontend/src/tests/productDetails/productDetail.test.tsx
@@ -2,28 +2,27 @@ import { describe, test, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import ProductDetail from '../../pages/product_detail/ProductDetail';
 
-describe('Product Detial page tests', () => {
-    const view = render(<ProductDetail/>);
-    expect(view).toBeTruthy();
+describe('Product Detail page tests', () => {
+        const view = render(<ProductDetail/>);
+        expect(view).toBeTruthy();
 
-    test('Check Productimg component exist', async () => {
-        const productimg = await screen.findByTestId('productimg');
-        expect(productimg).toBeDefined();
-    });
+        test('Check Productimg component exist', async () => {
+            const productimg = await screen.findByTestId('productimg');
+            expect(productimg).toBeDefined();
+        });
 
-    test('Check Pricecompare component exist', async () => {
-        const pricecompare = await screen.findByTestId('pricecompare');
-        expect(pricecompare).toBeDefined();
-    });
+        test('Check Pricecompare component exist', async () => {
+            const pricecompare = await screen.findByTestId('pricecompare');
+            expect(pricecompare).toBeDefined();
+        });
 
-    test('Check ProductReview component exist', async () => {
-        const productreview = await screen.findAllByTestId('productreview');
-        expect(productreview).toBeDefined();
-    });
+        test('Check ProductReview component exist', async () => {
+            const productreviews = await screen.findAllByTestId('productreview');
+            expect(productreviews.length).toBeGreaterThan(0);
+        });
 
-    test('Check ProductDetailParagraph component exist', async () => {
-        const pdp = await screen.findByTestId('pdp');
-        expect(pdp).toBeDefined();
+        // test('Check ProductDetailParagraph component exist', async () => {
+        //     const pdp = await screen.findByTestId('pdp');
+        //     expect(pdp).toBeInTheDocument();
+        // });
     });
-    
-});
