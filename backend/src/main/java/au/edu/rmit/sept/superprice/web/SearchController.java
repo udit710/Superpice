7f0d7294ad5d3c7619e69b638469ad12e6ba929b
Abhijeet Kumar <s3905291@student.rmit.edu.au>
2023-09-14 05:16:00 +1000

    update searchController to take data as param
    - /api/search?<data>=<value> is supported now
    - currently fetching just one type of them

--- backend/src/main/java/au/edu/rmit/sept/superprice/web/SearchController.java
+++ backend/src/main/java/au/edu/rmit/sept/superprice/web/SearchController.java
@@ -17,7 +17,39 @@ public class SearchController {
 
     @GetMapping
     public List<Product> getAllProducts(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String description,
+            @RequestParam(required = false) String lastUpdated,
+            @RequestParam(required = false) List<Long> storeIds,
+            @RequestParam(required = false) Double minPrice,
+            @RequestParam(required = false) Double maxPrice,
+            @RequestParam(required = false) String allergen) {
+
         List<Product> products = productService.getAllProducts();
+
+        if (name != null) {
+            products = productService.getProductsByProductName(name);
+        }
+
+        if (description != null) {
+            products = productService.getProductsByDescription(description);
+        }
+
+        if (lastUpdated != null) {
+            products = productService.getProductsByLastUpdated(lastUpdated);
+        }
+
+        if (storeIds != null && !storeIds.isEmpty()) {
+            products = productService.getProductsByStoreIds(storeIds);
+        }
+
+        if (minPrice != null && maxPrice != null) {
+            products = productService.getProductsByPriceBetween(minPrice, maxPrice);
+        }
+
+        if (allergen != null) {
+            products = productService.getProductsByAllergens(allergen);
+        }
 
         return products;
     }
