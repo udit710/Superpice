bfb47d9b1421f081a42541d5cc9eaeb4f047a71a
Abhijeet Kumar <s3905291@student.rmit.edu.au>
2023-09-14 05:06:35 +1000

    add other endpoints to search controller
    - /api/search/name
    - /api/search/description
    - /api/search/lastUpdated
    - /api/search/availability
    - /api/search/price
    - /api/search/allergens
    - /api/search/storeName
    - /api/search/storeIds

--- backend/src/main/java/au/edu/rmit/sept/superprice/web/SearchController.java
+++ backend/src/main/java/au/edu/rmit/sept/superprice/web/SearchController.java
@@ -26,4 +26,44 @@ public class SearchController {
     public Product getProductById(@PathVariable Long id) {
         return productService.getProductById(id);
     }
+
+    @GetMapping("/name/{productName}")
+    public List<Product> getProductByProductName(@PathVariable String productName) {
+        return productService.getProductsByProductName(productName);
+    }
+
+    @GetMapping("/description/{description}")
+    public List<Product> getProductsByDescription(@PathVariable String description) {
+        return productService.getProductsByDescription(description);
+    }
+
+    @GetMapping("/lastUpdated/{lastUpdated}")
+    public List<Product> getProductsByLastUpdated(@PathVariable String lastUpdated) {
+        return productService.getProductsByLastUpdated(lastUpdated);
+    }
+
+    @GetMapping("/availability")
+    public List<Product> getProductsByAvailability() {
+        return productService.getProductsByAvailability();
+    }
+
+    @GetMapping("/price")
+    public List<Product> getProductsByPriceBetween(@RequestParam Double minPrice, @RequestParam Double maxPrice) {
+        return productService.getProductsByPriceBetween(minPrice, maxPrice);
+    }
+
+    @GetMapping("/allergens/{allergen}")
+    public List<Product> getProductsByAllergens(@PathVariable String allergen) {
+        return productService.getProductsByAllergens(allergen);
+    }
+
+    @GetMapping("/storeName/{storeName}")
+    public List<Product> getProductsByStoreName(@PathVariable String storeName) {
+        return productService.getProductsByStoreName(storeName);
+    }
+
+    @GetMapping("/storeIds")
+    public List<Product> getProductsByStoreIds(@RequestParam List<Long> storeIds) {
+        return productService.getProductsByStoreIds(storeIds);
+    }
 }
