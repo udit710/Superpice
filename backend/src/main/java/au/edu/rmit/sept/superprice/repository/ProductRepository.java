60c9421a77da29c58f5fc94958a453b22e44ea7b
Abhijeet Kumar <s3905291@student.rmit.edu.au>
2023-09-14 04:49:55 +1000

    update findByProductName to return list of product

--- backend/src/main/java/au/edu/rmit/sept/superprice/repository/ProductRepository.java
+++ backend/src/main/java/au/edu/rmit/sept/superprice/repository/ProductRepository.java
@@ -6,13 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query("SELECT p FROM Product p WHERE p.productName = %?1%")
-    Optional<Product> findByProductName(String productName);
+    List<Product> findByProductName(String productName);
 
     @Query("SELECT p FROM Product p WHERE p.description LIKE %?1%")
     List<Product> findByDescriptionContaining(String description);
