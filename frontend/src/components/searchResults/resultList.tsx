169c9da0cc1986a2db8469952713b7ea0c8f7751
Vidyut Venkatesan <s3925040@student.rmit.edu.au>
2023-09-16 00:57:24 +1000

    Implement search products api in searchResults and resultList files

--- frontend/src/components/searchResults/resultList.tsx
+++ frontend/src/components/searchResults/resultList.tsx
@@ -1,9 +1,9 @@
 import React, { Component, ReactElement } from 'react';
 import './resultList.css';
 import logo from '../../logo.svg'; // Dummy image
+import { Product } from '../../pages/searchResults/searchResults';
 
-
-export default class ResultList extends Component {
+export default class ResultList extends Component<{products: Product[]}> {
   render() {
     const items = this.dataToElements(null);
     return (
@@ -16,21 +16,36 @@ export default class ResultList extends Component {
   dataToElements(data: string | null): ReactElement[] {
 		// TODO: Do something to get the data from string once apis are implemented
 
+    const products: Product[] = this.props.products;
+
 		const items: ReactElement[] = [];
 
-		const num = 10;
-		for (let i:number = 0; i < num; i++) {
-      items.push(<hr className='item-divider' />)
-			items.push(
-				<div className='item' id={'item-' + i} onClick={() => {alert("dummy item index " + i)}}>
-					<img src={logo} alt='logo' height='160' width='160' />
+		// const num = 10;
+		// for (let i:number = 0; i < num; i++) {
+    //   items.push(<hr className='item-divider' />)
+		// 	items.push(
+		// 		<div className='item' id={'item-' + i} onClick={() => {alert("dummy item index " + i)}}>
+		// 			<img src={logo} alt='logo' height='160' width='160' />
+    //       <div>
+    //         <p data-testid='product_name'>Dummy Item {i}</p>
+    //         <p data-testid='price'>$ 9.99</p>
+    //       </div>
+		// 		</div>
+		// 	);
+		// }
+
+    for (let i in products) {
+      items.push(<hr className='item-divider' />);
+      items.push(
+        <div className='item' onClick={() => {alert("dummy item index ")}} >
+          <img src={products[i].images[0].imageUrl} alt='product' height='160' width='160' />
           <div>
-            <p data-testid='product_name'>Dummy Item {i}</p>
-            <p data-testid='price'>$ 9.99</p>
+            <p data-testid='product_name'>{products[i].productName}</p>
+            <p data-testid='price'>$ {products[i].details[0].price}</p>
           </div>
-				</div>
-			);
-		}
+        </div>
+      )
+    }
 
 		return items;
 	}
--- frontend/src/pages/searchResults/searchResults.tsx
+++ frontend/src/pages/searchResults/searchResults.tsx
@@ -1,18 +1,45 @@
-import React, { Component } from 'react'
-import Subcategory from '../../components/searchResults/subcategory'
-import ResultList from '../../components/searchResults/resultList'
+import React, { Component } from 'react';
+import axios from 'axios';
+import Subcategory from '../../components/searchResults/subcategory';
+import ResultList from '../../components/searchResults/resultList';
+
+
+export interface Product {
+  productName: string;
+  description: string;
+  details: {store: {storeName: string}; price: number; available: number; discount: number }[];
+  images: { imageUrl: string }[];
+}
 
 export default class SearchResults extends Component<{searchItem: string}> {
+  state = {
+    products: [] as Product[],  
+  }
+
   render() {
     // console.log("testing " + this.props.searchItem);
+    // this.loadProductsList('milk'); // Get from url
     return (
       <div className='SearchResults'>
         <h3>Search Page</h3>
         <br/>
         <Subcategory/>
-        <ResultList/>
+        <ResultList products={this.state.products}/>
         
       </div>
     )
+  }
+
+  async componentDidMount() {
+    const searchTerm:string  = 'milk';
+    await axios.get(`http://localhost:8080/api/search?name=${searchTerm}`)
+      .then(res => {
+        console.log(res.data);
+        this.setState({ products: res.data });
+      })
+      .catch(err => {
+        console.error('Error while getting seach data: ', err);
+      });
+
   }
 }
