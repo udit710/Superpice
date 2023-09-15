5ff7b4feebffdfb23f9d02c4a2207d9bcd94ed03
ra5h1k <s3931830@student.rmit.edu.au>
2023-09-15 20:40:32 +1000

    Created ProductReview Component

--- frontend/src/components/product_review/ProductReview.tsx
+++ frontend/src/components/product_review/ProductReview.tsx
@@ -1,21 +1,22 @@
 import { Component } from "react";
 import "./ProductReview.css"
-export default class ProductReview extends Component{
-    render(){
 
-        //creating Fake Review
-        
+interface ProductReviewProps {
+    id: number;
+    rating: number;
+    body: string;
+}
 
-        return(
-            <div className='product_review_container' data-testid="productreview">
-                <div className="review_body">
-                    <h4>Person Name</h4>
-                    <p>Lorem ipsum dolor sit amet, cons commodo consequat. Duis 
-                        aute irure dolor in reprehenderit in voluptate velit esse 
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                         cupidatat non proident, sunt in culpa qui officia deserunt 
-                         mollit anim id est laborum.</p>
-                    <hr/>
+export default class ProductReview extends Component<ProductReviewProps>{
+
+    render() {
+        const {id, rating, body} = this.props;
+
+        return (
+            <div key={id} className="card mb-3" data-testid="productreview">
+                <div className="card-body">
+                    <h5 className="card-title">Rating: {rating}/5</h5>
+                    <p className="card-text">{body}</p>
                 </div>
             </div>
         )
