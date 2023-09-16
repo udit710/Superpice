f215589cb130bbec47ccc0fb35841481e6d6432b
Vidyut Venkatesan <s3925040@student.rmit.edu.au>
2023-09-17 05:32:52 +1000

    Added lombok generated to application main method

--- backend/src/main/java/au/edu/rmit/sept/superprice/SuperPriceApplication.java
+++ backend/src/main/java/au/edu/rmit/sept/superprice/SuperPriceApplication.java
@@ -3,9 +3,12 @@ package au.edu.rmit.sept.superprice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.Generated;
+
 @SpringBootApplication
 public class SuperPriceApplication {
 
+	@Generated
 	public static void main(String[] args) {
 		SpringApplication.run(SuperPriceApplication.class, args);
 	}
