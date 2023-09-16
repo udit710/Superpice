84a2cd89946857e69af37a859805bae3807e4050
IbrahimAshhab <104189991+IbrahimAshhab@users.noreply.github.com>
2023-09-16 10:51:10 +0000

    Add NotificationControllerTest

---
+++ backend/src/test/java/au/edu/rmit/sept/superprice/Controllers/NotificationControllerTest.java
@@ -0,0 +1,26 @@
+package au.edu.rmit.sept.superprice.Controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import au.edu.rmit.sept.superprice.model.Notification;
+import au.edu.rmit.sept.superprice.service.NotificationService;
+import au.edu.rmit.sept.superprice.web.NotificationController;
+
+@SpringBootTest
+public class OrderControllerTest {
+}
