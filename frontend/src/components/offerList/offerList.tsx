5867d523316dfb5a0f7cc4cd3039097199441143
ra5h1k <s3931830@student.rmit.edu.au>
2023-09-16 21:46:26 +1000

    Formatted the Offer Page and create routes

---
+++ frontend/src/components/offerList/offerList.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+
+export default class OfferList extends Component {
+    render() {
+        return(
+            <h3>Offer List: </h3>
+        )
+    }
+}
--- frontend/src/pages/homepage.tsx
+++ frontend/src/pages/homepage.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Offers from '../components/offers/offers';
 import './homepage.css';
+import { Outlet } from 'react-router';
 
 export default class Homepage extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class Homepage extends Component {
         <h1>SUPERPRICE</h1>
         <h5>Your place for shopping smart</h5>
         <Offers/>
+        <Outlet/>
       </div>
     )
   }
