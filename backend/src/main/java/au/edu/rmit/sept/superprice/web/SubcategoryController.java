a7b6d0ec1d3dc7bb9c1e41101ee261c46c68a629
Vidyut Venkatesan <s3925040@student.rmit.edu.au>
2023-09-22 03:20:51 +1000

    Added get subcategory by id api endpoint

---
+++ backend/src/main/java/au/edu/rmit/sept/superprice/web/SubcategoryController.java
@@ -0,0 +1,29 @@
+package au.edu.rmit.sept.superprice.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import au.edu.rmit.sept.superprice.model.SubCategory;
+import au.edu.rmit.sept.superprice.service.SubCategoryService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api/subcategory")
+public class SubcategoryController {
+    
+    private SubCategoryService subCategoryService;
+
+    @Autowired
+    public SubcategoryController(SubCategoryService subCategoryService) {
+        this.subCategoryService = subCategoryService;
+    }
+
+    @GetMapping("/{id}")
+    public SubCategory getReviewById(@PathVariable Long id) {
+        return subCategoryService.getById(id);
+    }
+}
