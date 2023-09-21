328ea72bbcb0a69e47e883610debd421b52f0bea
Abhijeet Kumar <s3905291@student.rmit.edu.au>
2023-09-21 17:21:14 +1000

    update sql migrations with lowercase table names
    - if required this could be used as alternative
    https://stackoverflow.com/a/44753093/13298307

--- backend/src/main/resources/db/migration/V1__schema.sql
+++ backend/src/main/resources/db/migration/V1__schema.sql
@@ -2,8 +2,8 @@
 -- CREATE DATABASE superprice;
 -- USE superprice;
 
--- ADDRESS table
-CREATE TABLE IF NOT EXISTS ADDRESS (
+-- address table
+CREATE TABLE IF NOT EXISTS address (
     address_id INT AUTO_INCREMENT PRIMARY KEY,
     address_line1 TEXT NOT NULL,
     address_line2 TEXT,
@@ -14,24 +14,24 @@ CREATE TABLE IF NOT EXISTS ADDRESS (
     address_type ENUM('USER', 'STORE', 'ORDER') NOT NULL
 );
 
--- CATEGORY table
-CREATE TABLE IF NOT EXISTS CATEGORY (
+-- category table
+CREATE TABLE IF NOT EXISTS category (
     category_id INT AUTO_INCREMENT PRIMARY KEY,
     category_name VARCHAR(255) NOT NULL UNIQUE
     );
 
--- SUB_CATEGORY association table
-CREATE TABLE IF NOT EXISTS SUB_CATEGORY (
+-- sub_category association table
+CREATE TABLE IF NOT EXISTS sub_category (
     sub_category_id INT AUTO_INCREMENT PRIMARY KEY,
 --     product_id INT, ERROR: Ask if we need to add this or not
     sub_category_name VARCHAR(255) NOT NULL UNIQUE,
     category_id INT,
 --     FOREIGN KEY (product_id) REFERENCES PRODUCT(product_id),
-    FOREIGN KEY (category_id) REFERENCES CATEGORY(category_id)
+    FOREIGN KEY (category_id) REFERENCES category(category_id)
     );
 
--- PRODUCT table
-CREATE TABLE IF NOT EXISTS PRODUCT (
+-- product table
+CREATE TABLE IF NOT EXISTS product (
     product_id INT AUTO_INCREMENT PRIMARY KEY,
     product_name VARCHAR(255) NOT NULL,
     sub_category_id INT,
@@ -39,30 +39,29 @@ CREATE TABLE IF NOT EXISTS PRODUCT (
     description TEXT,
     allergens TEXT,
     last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (sub_category_id) REFERENCES SUB_CATEGORY(sub_category_id),
---     Edit Product. java for this
-    FOREIGN KEY (category_id) REFERENCES CATEGORY(category_id)
+    FOREIGN KEY (sub_category_id) REFERENCES sub_category(sub_category_id),
+    FOREIGN KEY (category_id) REFERENCES category(category_id)
 );
 
--- PRODUCT_IMAGE table
-CREATE TABLE IF NOT EXISTS PRODUCT_IMAGE (
+-- product_image table
+CREATE TABLE IF NOT EXISTS product_image (
     image_id INT AUTO_INCREMENT PRIMARY KEY,
     product_id INT,
     image_url VARCHAR(255) NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES PRODUCT(product_id)
+    FOREIGN KEY (product_id) REFERENCES product(product_id)
 );
 
--- STORE table
-CREATE TABLE IF NOT EXISTS STORE (
+-- store table
+CREATE TABLE IF NOT EXISTS store (
     store_id INT AUTO_INCREMENT PRIMARY KEY,
     store_name VARCHAR(255) NOT NULL,
     address_id INT,
     contact_details VARCHAR(255),
-    FOREIGN KEY (address_id) REFERENCES ADDRESS(address_id)
+    FOREIGN KEY (address_id) REFERENCES address(address_id)
 );
 
--- APP_USER table
-CREATE TABLE IF NOT EXISTS APP_USER (
+-- app_user table
+CREATE TABLE IF NOT EXISTS app_user (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
@@ -71,22 +70,22 @@ CREATE TABLE IF NOT EXISTS APP_USER (
     last_name VARCHAR(255) NOT NULL,
     phone VARCHAR(15),
     address_id INT,
-    FOREIGN KEY (address_id) REFERENCES ADDRESS(address_id)
+    FOREIGN KEY (address_id) REFERENCES address(address_id)
 );
 
--- REVIEW table
-CREATE TABLE IF NOT EXISTS REVIEW (
+-- review table
+CREATE TABLE IF NOT EXISTS review (
     review_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     product_id INT,
     rating INT NOT NULL,
     comment TEXT,
-    FOREIGN KEY (user_id) REFERENCES APP_USER(user_id),
-    FOREIGN KEY (product_id) REFERENCES PRODUCT(product_id)
+    FOREIGN KEY (user_id) REFERENCES app_user(user_id),
+    FOREIGN KEY (product_id) REFERENCES product(product_id)
 );
 
--- ORDERS table
-CREATE TABLE IF NOT EXISTS ORDERS (
+-- orders table
+CREATE TABLE IF NOT EXISTS orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -94,43 +93,43 @@ CREATE TABLE IF NOT EXISTS ORDERS (
     total_amount DECIMAL(10, 2) NOT NULL,
     order_address_id INT,
     payment_method ENUM('CREDIT_CARD','DEBIT_CARD','PAYPAL','WALLET'),
-    FOREIGN KEY (user_id) REFERENCES APP_USER(user_id),
-    FOREIGN KEY (order_address_id) REFERENCES ADDRESS(address_id)
+    FOREIGN KEY (user_id) REFERENCES app_user(user_id),
+    FOREIGN KEY (order_address_id) REFERENCES address(address_id)
 );
 
--- ORDERS_ITEM table
-CREATE TABLE IF NOT EXISTS ORDERS_ITEM (
+-- orders_item table
+CREATE TABLE IF NOT EXISTS orders_item (
     order_item_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     product_id INT,
     quantity INT NOT NULL,
     sub_total DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-    FOREIGN KEY (product_id) REFERENCES PRODUCT(product_id)
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES product(product_id)
 );
 
--- NOTIFICATION table
-CREATE TABLE IF NOT EXISTS NOTIFICATION (
+-- notification table
+CREATE TABLE IF NOT EXISTS notification (
     notification_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     message TEXT NOT NULL,
     type ENUM('INFO', 'WARNING', 'ERROR'),
     date_sent TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES APP_USER(user_id)
+    FOREIGN KEY (user_id) REFERENCES app_user(user_id)
 );
 
--- CART_ITEM table
-CREATE TABLE IF NOT EXISTS CART_ITEM (
+-- cart_item table
+CREATE TABLE IF NOT EXISTS cart_item (
     cart_item_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     product_id INT,
     quantity INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES APP_USER(user_id),
-    FOREIGN KEY (product_id) REFERENCES PRODUCT(product_id)
+    FOREIGN KEY (user_id) REFERENCES app_user(user_id),
+    FOREIGN KEY (product_id) REFERENCES product(product_id)
 );
 
--- PRODUCT_DETAILS association table
-CREATE TABLE IF NOT EXISTS PRODUCT_DETAILS (
+-- product_details association table
+CREATE TABLE IF NOT EXISTS product_details (
     product_details_id INT AUTO_INCREMENT PRIMARY KEY,
     product_id INT,
     store_id INT,
@@ -138,31 +137,7 @@ CREATE TABLE IF NOT EXISTS PRODUCT_DETAILS (
     discount INT NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     available INT NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES PRODUCT(product_id),
-    FOREIGN KEY (store_id) REFERENCES STORE(store_id),
+    FOREIGN KEY (product_id) REFERENCES product(product_id),
+    FOREIGN KEY (store_id) REFERENCES store(store_id),
     CHECK (discount>=0 AND discount<=100)
 );
-
--- -- ORDER table
--- CREATE TABLE IF NOT EXISTS ORDERS (
---     order_id INT AUTO_INCREMENT PRIMARY KEY,
---     user_id INT,
---     order_id INT NOT NULL,
---     -- order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---     -- total_amount DECIMAL(10, 2) NOT NULL,
---     -- order_address_id INT,
---     -- FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
---     FOREIGN KEY (user_id) REFERENCES APP_USER(user_id)
---     -- FOREIGN KEY (order_address_id) REFERENCES ADDRESS(address_id)
--- );
-
--- -- ORDER_ITEM table (to store product items in orders along with their quantity)
--- CREATE TABLE IF NOT EXISTS ORDER_ITEM (
---     order_item_id INT AUTO_INCREMENT PRIMARY KEY,
---     order_id INT,
---     product_store_id INT,
---     quantity INT NOT NULL,
---     sub_total DECIMAL(10, 2) NOT NULL,
---     FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
---     FOREIGN KEY (product_store_id) REFERENCES PRODUCT_STORE(product_store_id)
--- );
--- backend/src/main/resources/db/migration/V2__data.sql
+++ backend/src/main/resources/db/migration/V2__data.sql
@@ -1,21 +1,21 @@
--- Populating ADDRESS table
-INSERT INTO ADDRESS (address_line1, address_line2, city, state, postal_code, country, address_type)
+-- Populating address table
+INSERT INTO address (address_line1, address_line2, city, state, postal_code, country, address_type)
 VALUES ('123 Main St', 'Apt 4B', 'Melbourne', 'VIC', '3000', 'Australia', 'STORE'),
        ('456 Elm St', 'Suite 12A', 'Sydney', 'NSW', '2000', 'Australia', 'USER'),
        ('789 Maple St', 'Floor 3', 'Brisbane', 'QLD', '4000', 'Australia', 'ORDER'),
        ('101 Pine St', NULL, 'Adelaide', 'SA', '5000', 'Australia', 'STORE'),
        ('202 Oak St', 'Apt 2C', 'Perth', 'WA', '6000', 'Australia', 'USER');
 
--- Populating CATEGORY table
-INSERT INTO CATEGORY (category_name)
+-- Populating category table
+INSERT INTO category (category_name)
 VALUES ('Dairy'),
        ('Beverages'),
        ('Fruits'),
        ('Vegetables'),
        ('Bakery');
 
--- Populating SUB_CATEGORY association table
-INSERT INTO SUB_CATEGORY (sub_category_name, category_id)
+-- Populating sub_category association table
+INSERT INTO sub_category (sub_category_name, category_id)
 VALUES ('Almond Milk', 1),
        ('Oat Milk', 1),
        ('Soy Milk', 1),
@@ -28,24 +28,24 @@ VALUES ('Almond Milk', 1),
        ('Fresh Baker', 5),
        ('Packaged Baker', 5);
 
--- Populating STORE table
-INSERT INTO STORE (store_name, address_id)
+-- Populating store table
+INSERT INTO store (store_name, address_id)
 VALUES ('Coles', 1),
        ('Woolworths', 4),
        ('Aldi', 1),
        ('IGA', 4),
        ('Foodland', 4);
 
--- Populating PRODUCT table
-INSERT INTO PRODUCT (product_name, description, allergens, sub_category_id, category_id)
+-- Populating product table
+INSERT INTO product (product_name, description, allergens, sub_category_id, category_id)
 VALUES ('Skim Milk', 'Low fat milk', 'Lactose', 5, 1),
        ('Full Cream Milk', 'Rich and creamy milk', 'Lactose', 4, 1),
        ('Orange Juice', 'Freshly squeezed orange juice', NULL, 6, 2),
        ('Apples', 'Crisp and sweet', NULL, 9, 3),
        ('Bread', 'Freshly baked', 'Gluten', 10, 5);
 
--- Populating PRODUCT_IMAGE table
-INSERT INTO PRODUCT_IMAGE (product_id, image_url)
+-- Populating product_image table
+INSERT INTO product_image (product_id, image_url)
 VALUES (1, 'https://source.unsplash.com/random?milk'),
        (1, 'https://source.unsplash.com/random?milk'),
        (1, 'https://source.unsplash.com/random?milk'),
@@ -59,47 +59,47 @@ VALUES (1, 'https://source.unsplash.com/random?milk'),
        (5, 'https://source.unsplash.com/random?bread'),
        (5, 'https://source.unsplash.com/random?bread');
 
--- Populating APP_USER table
-INSERT INTO APP_USER (username, password, email, first_name, last_name, phone, address_id)
+-- Populating app_user table
+INSERT INTO app_user (username, password, email, first_name, last_name, phone, address_id)
 VALUES ('john_doe', 'password123', 'john.doe@example.com', 'John', 'Doe', '+1234567890', 2),
        ('jane_smith', 'securepass456', 'jane.smith@example.com', 'Jane', 'Smith', '+0987654321', 5);
 
--- Populating REVIEW table
-INSERT INTO REVIEW (user_id, product_id, rating, comment)
+-- Populating review table
+INSERT INTO review (user_id, product_id, rating, comment)
 VALUES (1, 1, 5, 'Great milk!'),
        (1, 2, 4, 'Tastes good.'),
        (2, 3, 5, 'Refreshing!'),
        (2, 4, 4, 'Crisp apples.'),
        (2, 1, 5, 'Really tasty milk!');
 
--- Populating ORDERS table
-INSERT INTO ORDERS (user_id,order_status,total_amount,order_address_id,payment_method)
+-- Populating orders table
+INSERT INTO orders (user_id,order_status,total_amount,order_address_id,payment_method)
 VALUES (1,'PENDING',7.00,3,'CREDIT_CARD'),
        (2,'SHIPPED',6.00,3,'CREDIT_CARD');
 
 
--- Populating ORDERS_ITEM table
-INSERT INTO ORDERS_ITEM (order_id, product_id, quantity, sub_total)
+-- Populating orders_item table
+INSERT INTO orders_item (order_id, product_id, quantity, sub_total)
 VALUES (1, 1, 2, 3.00),
        (1, 2, 2, 4.00),
        (2, 3, 1, 3.00),
        (2, 4, 3, 1.50);
 
--- Populating NOTIFICATION table
-INSERT INTO NOTIFICATION (user_id, message, type)
+-- Populating notification table
+INSERT INTO notification (user_id, message, type)
 VALUES (1, 'Your order has been shipped!', 'INFO'),
        (2, 'Your order is on its way!', 'INFO');
 
--- Populating CART_ITEM table
-INSERT INTO CART_ITEM (user_id, product_id, quantity)
+-- Populating cart_item table
+INSERT INTO cart_item (user_id, product_id, quantity)
 VALUES (1, 1, 2),
        (1, 2, 1),
        (2, 3, 1),
        (2, 4, 3),
        (2, 5, 1);
 
--- Populating PRODUCT_STORE association table
-INSERT INTO PRODUCT_DETAILS (product_id, store_id, original_price, discount, price, available)
+-- Populating product_details association table
+INSERT INTO product_details (product_id, store_id, original_price, discount, price, available)
 VALUES (1, 1, 1.50, 30, 1.05, 10),
        (1, 2, 1.60, 45, 0.88, 5),
        (1, 3, 1.55, 20, 1.24, 8),
