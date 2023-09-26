d4d5db8ae7fce0cefd79d5a5e056d279abbf2b4b
Vidyut Venkatesan <103317060+Vidyut03@users.noreply.github.com>
2023-09-26 19:40:19 +1000

    implement delete review

--- frontend/src/components/product_review/ProductReview.tsx
+++ frontend/src/components/product_review/ProductReview.tsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import "./ProductReview.css"
+import axios from "axios";
 
 interface ProductReviewProps {
     id: number;
@@ -8,6 +9,9 @@ interface ProductReviewProps {
 }
 
 export default class ProductReview extends Component<ProductReviewProps>{
+    state = {
+        review: null as string | null,
+    };
 
     render() {
         const {id, rating, body} = this.props;
@@ -17,8 +21,54 @@ export default class ProductReview extends Component<ProductReviewProps>{
                 <div className="card-body">
                     <h5 className="card-title" data-testid="rating" >Rating: {rating}/5</h5>
                     <p className="card-text" data-testid="comment" >{body}</p>
+
+                    <div className="dropdown">
+                        <a className="btn btn-secondary dropdown-toggle" href="/#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown link
+                        </a>
+
+                        <ul className="dropdown-menu">
+                            <li><a className="dropdown-item" href="/#" data-bs-toggle="modal" data-bs-target="#edit-review">Edit</a></li>
+                            <li><a className="dropdown-item" href="/#" onClick={this.onDelete}>Delete</a></li>
+
+                        </ul>
+
+
+                        <div className="modal fade" id="edit-review" tabIndex={-1} aria-labelledby="reviewLabel" aria-hidden="true">
+                                <div className="modal-dialog modal-dialog-centered">
+                                    <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h1 className="modal-title fs-5" id="edit-review-modal-title">Edit Review</h1>
+                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div className="modal-body">
+                                        <form id='edit-review-form'>
+                                            <label id='edit-review-lable' className="form-label" htmlFor='review-text'>New Review</label>
+                                            <input className="form-control" type='text' name='comment' id='review-text' onChange={this.handleChangeText} placeholder='New Review' required />
+                                        </form>
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="submit" className="btn btn-primary" form='edit-review-form'>Add Review</button>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
                 </div>
             </div>
         )
+    }
+
+    handleChangeText = (e: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({ review: e.target.value });
+    }
+
+    onDelete = () => {
+        console.log(this.props.id);
+        axios.delete(`${process.env.REACT_APP_API_URL}/api/reviews/${this.props.id}`)
+        .then(res => {
+            window.location.reload();
+        });
     }
 }
