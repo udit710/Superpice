6815d8b69f258734c5f485a398d8d7e895c10055
IbrahimAshhab <s3953973@student.rmit.edu.au>
2023-09-29 16:39:21 +1000

    fix notification merge

--- backend/src/main/resources/db/migration/V1__schema.sql
+++ backend/src/main/resources/db/migration/V1__schema.sql
@@ -113,7 +113,7 @@ CREATE TABLE IF NOT EXISTS notification (
     notification_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     message TEXT NOT NULL,
-    type ENUM('INFO', 'WARNING', 'ERROR'),
+    type ENUM('OFFERS', 'ORDER', 'ERROR'),
     date_sent TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES app_user(user_id)
 );
