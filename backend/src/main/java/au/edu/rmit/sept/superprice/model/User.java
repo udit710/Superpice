f5c1169f0ec2d14c1f0e1786f4a0c0d076ee1672
Vidyut Venkatesan <103317060+Vidyut03@users.noreply.github.com>
2023-09-30 19:32:39 +1000

    Added spring security jwt config

--- backend/src/main/java/au/edu/rmit/sept/superprice/model/User.java
+++ backend/src/main/java/au/edu/rmit/sept/superprice/model/User.java
@@ -10,6 +10,8 @@ import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Entity
@@ -23,24 +25,45 @@ public class User {
     @Column(name = "user_id")
     private Long userId;
 
+    @Setter
     @Column(name = "username", nullable = false, unique=true)
     private String username;
 
+    @Setter
     @Column(name = "password", nullable = false)
     private String password;
     
+    @Setter
     @Column(name = "email", nullable = false, unique=true)
     private String email;
 
+    @Setter
     @Column(name = "first_name", nullable = false)
     private String firstName;
 
+    @Setter
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
+    @Setter
     @Column(name = "phone")
     private String phone;
 
+    @Setter
     @Column(name = "address_id")
     private Long addressId;
+
+    public User(String email) {
+        this.email = email;
+    }
+
+    public User(String username, String password, String email, String firstName, String lastName, String phone, Long addressId) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.addressId = addressId;
+    }
 }
