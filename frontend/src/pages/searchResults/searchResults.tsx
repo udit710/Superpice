618f85e9e9f9052584fb4c9f54727fab60f19a0f
Vidyut Venkatesan <103317060+Vidyut03@users.noreply.github.com>
2023-10-04 01:50:04 +1100

    Fixed problem causing categories to not show up when logged in

--- frontend/src/pages/searchResults/searchResults.tsx
+++ frontend/src/pages/searchResults/searchResults.tsx
@@ -4,7 +4,6 @@ import Subcategory from '../../components/searchResults/subcategory';
 import ResultList from '../../components/searchResults/resultList';
 import { Product } from '../../interfaces/product.interface';
 import { SubCategory } from '../../interfaces/subcategory.interface';
-// import { URLSearchParams } from 'url';
 
 export default class SearchResults extends Component<{searchItem: string}> {
   state = {
@@ -31,34 +30,36 @@ export default class SearchResults extends Component<{searchItem: string}> {
 
     await axios.get(`${process.env.REACT_APP_API_URL}/api/search?name=${searchTerm}`)
       .then(res => {
-        this.setState({ products: res.data });
+        this.setState({ products: res.data }, this.getSubcategories);        
       })
       .catch(err => {
         console.error('Error while getting seach data: ', err);
       });
-    
+  }
+
+  getSubcategories = async () => {
     // Gets all subcategory ids
     const products: Product[] = this.state.products;
+    console.log(products);
+
     const subs_ids: number[] = [];
     for (let p in products) {
-      // console.log(products[p].subCategoryId);
+      console.log(products[p].subCategoryId);
       subs_ids.push(products[p].subCategoryId);
     }
 
     // Gets required subcategories
     const subs: SubCategory[] = [];
-		for (let i in subs_ids) {
-			await axios.get(`${process.env.REACT_APP_API_URL}/api/subcategory/${subs_ids[i]}`)
-      		.then(res => {
-				console.log(res.data);
-        		subs.push(res.data);
-      		})
-      		.catch(err => {
-        		console.error('Error while getting subcategory: ', err);
-      		});
-		}
-		this.setState({ subcategories: subs })
-
-
+    for (let i in subs_ids) {
+      await axios.get(`${process.env.REACT_APP_API_URL}/api/subcategory/${subs_ids[i]}`)
+          .then(res => {
+        console.log(res.data);
+            subs.push(res.data);
+          })
+          .catch(err => {
+            console.error('Error while getting subcategory: ', err);
+          });
+    }
+    this.setState({ subcategories: subs })
   }
 }
