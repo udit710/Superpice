9e4feb657e638c66df4fc2be68bcde9232f61566
Vidyut Venkatesan <103317060+Vidyut03@users.noreply.github.com>
2023-10-04 03:37:02 +1100

    Added minimum discount slider

--- frontend/src/pages/offers_page/offersPage.tsx
+++ frontend/src/pages/offers_page/offersPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import { Product } from '../../interfaces/product.interface'
 import axios from 'axios'
 import OffersCard from '../../components/offers_card/offersCard';
@@ -8,9 +8,13 @@ interface maxProductDiscount {
 	product: Product;
 	maxDiscount: number;
 }
+
 export default function OffersPage() {
+	const DEFAULT_MIN_OPTION = 30;
+
 	const [products, setProducts] = useState([] as Product[]);
 	const [maxDiscount, setMaxDiscount] = useState([] as maxProductDiscount[]);
+	const [minDiscountOption, setMinDiscountOption] = useState(DEFAULT_MIN_OPTION as number);
 	
 	useEffect(() => {
 		axios.get(`${process.env.REACT_APP_API_URL}/api/products/discounts`)
@@ -41,10 +45,18 @@ export default function OffersPage() {
 	return (
 		<div className='OffersPage'>
 			<h1>Current Offers</h1>
+
+			<label htmlFor="min-discount" className="form-label min-label">Minimum Discount</label><br/>
+			<input type="range" className="form-range" min="0" max="100" id="min-discount" defaultValue={minDiscountOption} onChange={e => {setMinDiscountOption(+e.target.value)}} />
+			<p>{ minDiscountOption }%</p>
+
 			<div className='offers-cards'>
-				{maxDiscount.map(p => (
-					<OffersCard product={p.product} maxIndex={p.maxDiscount} />
-				))}
+				{maxDiscount.map(p => {
+					if (p.product.details[p.maxDiscount].discount >= minDiscountOption)
+						return (
+							<OffersCard product={p.product} maxIndex={p.maxDiscount} />
+						)
+				})}
 			</div>
 			
 		</div>
