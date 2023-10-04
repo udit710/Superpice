bb1edea2e513b4da821ac30b5bb7b921472d066b
IbrahimAshhab <104189991+IbrahimAshhab@users.noreply.github.com>
2023-10-04 09:57:39 +0000

    removed old code

--- frontend/src/components/navbar/notifications.tsx
+++ frontend/src/components/navbar/notifications.tsx
@@ -2,33 +2,6 @@ import React, { Component } from 'react';
 import './notifications.css';
 
 export default class Notifications extends Component {
-  // constructor(props: any) {
-  //   super(props);
-  //   this.toggleNotificationTypes = this.toggleNotificationTypes.bind(this);
-  // }
-
-  // componentDidMount(): void {
-  //     const notifications =  document.querySelector('.Notifications') as Element | null;
-  //     notifications?.addEventListener('click', function(event) {
-  //       event.stopPropagation();
-  //    });
-  // }
-  toggleNotificationTypes(e: any) {
-    console.log(e.target.value);
-    const types: string[] = ["discount", "order", "account"];
-
-    if (e.target.value === null || !(e.target.value in types)) return;
-
-    for (let t in types) {
-      const notifs = document.getElementById(t + "-notifs")?.style as CSSStyleDeclaration;
-      if (t === e.target.value) {
-        notifs.display = "block";
-      }
-      else {
-        notifs.display = "none";
-      }
-    }
-  }
 
   render() {
     return (
