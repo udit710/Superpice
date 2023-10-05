799ea10b4c9cd89935889e133ac5f1ad0b3c70b5
Vidyut Venkatesan <103317060+Vidyut03@users.noreply.github.com>
2023-10-05 19:45:49 +1100

    Product service test

---
+++ backend/src/test/java/au/edu/rmit/sept/superprice/Models/CartItemTest.java
@@ -0,0 +1,44 @@
+package au.edu.rmit.sept.superprice.Models;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import au.edu.rmit.sept.superprice.model.CartItem;
+import au.edu.rmit.sept.superprice.model.ProductDetails;
+import au.edu.rmit.sept.superprice.model.User;
+
+@SpringBootTest
+public class CartItemTest {
+    
+    CartItem cartItem;
+
+    @BeforeEach
+    void createReview() {
+        this.cartItem = new CartItem(1l, new ProductDetails(), new User(), 1);
+    }
+
+    @Test
+    void should_return_id() {
+        assertEquals(1l, this.cartItem.getId());
+    }
+
+    @Test
+    void should_return_productDetails() {
+        assertNotNull(this.cartItem.getProductDetailsId());
+    }
+    
+    @Test
+    void should_return_user() {
+        assertNotNull(this.cartItem.getUserId());
+    }
+
+    @Test
+    void should_return_qty() {
+        assertEquals(1, this.cartItem.getQuantity());
+    }
+
+}
