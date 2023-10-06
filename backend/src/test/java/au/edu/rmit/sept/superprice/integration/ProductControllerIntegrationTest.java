14c19d4e09b455666c301f5893f6886b1a761b07
Vidyut Venkatesan <103317060+Vidyut03@users.noreply.github.com>
2023-10-07 01:44:50 +1100

    product integration tests

---
+++ backend/src/test/java/au/edu/rmit/sept/superprice/integration/ProductControllerIntegrationTest.java
@@ -0,0 +1,124 @@
+package au.edu.rmit.sept.superprice.integration;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import au.edu.rmit.sept.superprice.SuperPriceApplication;
+import au.edu.rmit.sept.superprice.model.Product;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = SuperPriceApplication.class)
+public class ProductControllerIntegrationTest {
+    
+    @Autowired
+    MockMvc mvc;
+    @Autowired
+    Flyway flyway;
+
+    @BeforeEach
+    private void init() {
+        flyway.migrate();
+    }
+
+    @AfterEach
+    private void clean() {
+        flyway.clean();
+    }
+
+    @Test
+    void get_all_products() throws Exception {
+        mvc.perform(get("/api/products").contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+
+            .andExpect(jsonPath("$[0].id", is(1)))
+            .andExpect(jsonPath("$[0].productName", is("Skim Milk")));
+    }
+
+    @Test
+    void get_product_by_id_1() throws Exception {
+        mvc.perform(get("/api/products/1").contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+
+            .andExpect(jsonPath("$.id", is(1)))
+            .andExpect(jsonPath("$.productName", is("Skim Milk")));
+    }
+
+    @Test
+    void create_product() throws Exception {
+        mvc.perform(post("/api/products")
+            .content(ObjectToJson.asJsonString(new Product(null, "test", null, null, null, null, null, null, null)))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void update_product_1() throws Exception {
+        mvc.perform(put("/api/products/1")
+            .content(ObjectToJson.asJsonString(new Product(1l, "test", null, null, null, null, null, null, null)))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+
+            .andExpect(jsonPath("$.productName", is("test")));
+    }
+
+    // @Test
+    // void delete_product() throws Exception {
+    //     mvc.perform(delete("/api/products/1")).andExpect(status().isOk());
+    // }
+
+    @Test
+    void get_product_by_CategoryID_1() throws Exception {
+        mvc.perform(get("/api/products/category/id/1").contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+
+    }
+
+    @Test
+    void get_product_by_CategoryName_() throws Exception {
+        mvc.perform(get("/api/products/category/name/Dairy").contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+
+    }
+    
+    @Test
+    void get_product_by_discount() throws Exception {
+        mvc.perform(get("/api/products/offer/50").contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void get_products_that_have_discounts() throws Exception {
+        mvc.perform(get("/api/products/discounts").contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+}
