e66646c8935f3d88f7b2c618373b7413d6dbafd2
Vidyut Venkatesan <103317060+Vidyut03@users.noreply.github.com>
2023-10-08 13:05:27 +1100

    Added more model tests

--- backend/src/test/java/au/edu/rmit/sept/superprice/Models/ProductDetailsTest.java
+++ backend/src/test/java/au/edu/rmit/sept/superprice/Models/ProductDetailsTest.java
@@ -18,42 +18,49 @@ public class ProductDetailsTest {
 
     @BeforeEach
     void createProductDetails() {
-        this.productDetails = new ProductDetails(1l, new Product(), new Store(), 1.0, 1, 1.0, 1);
+        this.productDetails = new ProductDetails(1l, null, null, 1.0, 1, 1.0, 1);
     }
 
     @Test
     void should_return_id() {
-        assertEquals(1l, this.productDetails.getId());
+        this.productDetails.setId(2l);
+        assertEquals(2l, this.productDetails.getId());
     }
 
     @Test
     void should_return_product() {
+        this.productDetails.setProduct(new Product());
         assertNotNull(this.productDetails.getProduct());
     }
 
     @Test
     void should_return_Store() {
+        this.productDetails.setStore(new Store());
         assertNotNull(this.productDetails.getStore());
     }
 
     @Test
     void should_return_Original_price() {
-        assertEquals(1.0, this.productDetails.getOriginal_price());
+        this.productDetails.setOriginal_price(2.0);
+        assertEquals(2.0, this.productDetails.getOriginal_price());
     }
 
     @Test
     void should_return_Discount() {
-        assertEquals(1, this.productDetails.getDiscount());
+        this.productDetails.setDiscount(2);
+        assertEquals(2, this.productDetails.getDiscount());
     }
 
     @Test
     void should_return_Price() {
-        assertEquals(1.0, this.productDetails.getPrice());
+        this.productDetails.setPrice(2d);
+        assertEquals(2d, this.productDetails.getPrice());
     }
 
     @Test
     void should_return_Available() {
-        assertEquals(1, this.productDetails.getAvailable());
+        this.productDetails.setAvailable(2);
+        assertEquals(2, this.productDetails.getAvailable());
     }
 
 }
